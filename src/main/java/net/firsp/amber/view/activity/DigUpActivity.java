69198845c0ca8f887c2d7c5721d7b9141ca4e738
FirstSpring <firstspring1845@gmail.com>
2014-07-20 14:03:32 +0900

    掘り返し機能付けた

---
+++ src/main/java/net/firsp/amber/view/activity/DigUpActivity.java
@@ -0,0 +1,61 @@
+package net.firsp.amber.view.activity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+
+import net.firsp.amber.account.Accounts;
+import net.firsp.amber.util.DialogUtil;
+import net.firsp.amber.util.UIHandler;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class DigUpActivity extends Activity {
+
+    //Configurationのほうで自分のRTを含ませる設定をしておく
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(android.R.style.Theme_Translucent);
+
+        Intent intent = getIntent();
+
+        final ProgressDialog d = DialogUtil.createProgress(this);
+        d.show();
+
+        try{
+            final long id = Long.parseLong(intent.getData().toString().replaceAll(".*/",""));
+            Accounts.initialize(this);
+            new Thread(){
+                @Override
+                public void run(){
+                    try{
+                        Twitter t = Accounts.getInstance().getDefaultAccount().getTwitter();
+                        Status s = t.showStatus(id);
+                        //とりあえず削除を試みる
+                        try{
+                            t.destroyStatus(s.getCurrentUserRetweetId());
+                        }catch(Exception e){
+                        }
+                        t.retweetStatus(id);
+                    }catch(Exception e){
+                    }
+                    new UIHandler(){
+                        @Override
+                        public void run(){
+                            d.dismiss();
+                            finish();
+                        }
+                    };
+
+                }
+            }.start();
+        }catch(Exception e){
+            finish();
+        }
+    }
+}
